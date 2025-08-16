ec4188bb71259fe23472cd71b8267a07bfca8859
x40arcade <sobrepenakenneth97@gmail.com>
2025-08-16 14:21:08 +0800

    BarangaySports 16/08/25
    added try catch fix if user inputs an String to an int

--- BarangaySports/src/Main.java
+++ BarangaySports/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		// Variable Declaration
-		int userInput;
+		int sportInput = 0, colorInput1, colorInput2;
 		final int SPORTS_OPTIONS = 2, COLOR_OPTIONS = 6;
 		
 		int team1;
@@ -46,36 +46,48 @@ public class Main {
 		//Break
 		System.out.println("-----------------------");
 		
-		// Ask the user
-		System.out.print("Enter number of the sport: ");
-		userInput = Integer.parseInt(reader.readLine());
+		try {
+			// Ask the user
+			System.out.print("Enter number of the sport: ");
+			sportInput = Integer.parseInt(reader.readLine());
+
+			}	catch (Exception e) {
+				System.out.println("-----------------------");
+				System.out.println("Please enter an whole number only! ");
+				System.out.println("-----------------------");
+				System.out.print("Enter number of the sport: ");
+				sportInput = Integer.parseInt(reader.readLine());
+			}	
 		
 		//Break
 		System.out.println("-----------------------");
 		 
-		if (userInput > SPORTS_OPTIONS) {
+		// Sports process
+		if (sportInput > SPORTS_OPTIONS) {
 			System.out.println("Please only select the number listed on the index!");
 			//Break
 			System.out.println("-----------------------");
 			System.out.print("Enter number of the sport: ");
 			// Reader
-			userInput = Integer.parseInt(reader.readLine());
-		} else if (userInput < 0){
+			sportInput = Integer.parseInt(reader.readLine());
+		} else if (sportInput < 0){
 			System.out.println("Please only select the number listed on the index!");
 			//Break
 			System.out.println("-----------------------");
 			System.out.print("Enter number of the sport: ");
 			// Reader
-			userInput = Integer.parseInt(reader.readLine());	
+			sportInput = Integer.parseInt(reader.readLine());	
 		} else {
-			System.out.println("You choose: " + sports[userInput]);
+			System.out.println("You choose: " + sports[sportInput]);
 			//Break
 			System.out.println("-----------------------");
-			System.out.println("- Pick a team color - ");
+			System.out.println("-----------------------");
+			System.out.println("- Pick a color - ");
 			//Break
 			System.out.println("-----------------------");
-		}	
-		//Team Color
+			}
+		
+		//Team1 Color
 		System.out.println(" [0] YELLOW ");
 		System.out.println(" [1] BLUE ");
 		System.out.println(" [2] RED ");
@@ -84,8 +96,53 @@ public class Main {
 		System.out.println(" [5] PINK ");
 		//Break
 		System.out.println("-----------------------");
-		System.out.print("Enter color of the team: ");
+		
+		try {
+			// Ask user to input team1
+			System.out.print("Enter color of the team1: ");
+			// Reader
+			colorInput1 = Integer.parseInt(reader.readLine());
+
+
+			}	catch (Exception e) {
+				System.out.println("-----------------------");
+				System.out.println("Please enter an whole number only! ");
+				System.out.println("-----------------------");
+				System.out.print("Enter color of the team1: ");
+				colorInput1 = Integer.parseInt(reader.readLine());
+			} 
+		
+
+		//Break
+		System.out.println("-----------------------");
+		
+		// team 1 color picked
+		if (colorInput1 > COLOR_OPTIONS) {
+			System.out.println("Please only select the number listed on the index");
+			//Break
+			System.out.println("-----------------------");
+			colorInput1 = Integer.parseInt(reader.readLine());
+		} else if (colorInput1 < 0){
+			System.out.println("Please only select the number listed on the index");
+			//Break
+			System.out.println("-----------------------");
+			colorInput1 = Integer.parseInt(reader.readLine());
+		} else {
+			System.out.println("Team 1 picked color: " + teamColor[colorInput1]);
+			System.out.println("-----------------------");
+		}
+		
+		// Ask user to input team2
+		System.out.print("Enter color of the team2: ");
 		// Reader
-		userInput = Integer.parseInt(reader.readLine());
+		colorInput2 = Integer.parseInt(reader.readLine());
+		System.out.println("-----------------------");
+		System.out.println("Team 1 picked color: " + teamColor[colorInput2]) ;
+		System.out.println("-----------------------");
+		// VERSUS
+		System.out.println("=====================================");
+		System.out.println("Team " + teamColor[colorInput1] + " Vs " + "Team " + teamColor[colorInput2]);
+		System.out.println("=====================================");
+		
 	}
 }
