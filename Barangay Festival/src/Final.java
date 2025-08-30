c94753b4fdade390fc6b25853a956624c5cb161d
x40arcade <sobrepenakenneth97@gmail.com>
2025-08-30 22:07:58 +0800

    30/08/25 Final

--- Barangay Festival/src/Final.java
+++ Barangay Festival/src/Final.java
@@ -70,9 +70,8 @@ public class Final {
 		// >>>>> ------------------- FOOD STALL ------------------- <<<<<
 		// Step 12
 		System.out.println(">>>>> - Welcome to the Food Stall - <<<<<");
-		// Step 13
 		System.out.println(" - Are you ready to choose your meal? -");
-		// Step 14
+		// Step 13
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^^^^^^^^^ - Food Stall Menu - ^^^^^^^^^^^^");
 		System.out.println("--------------------------------------------------");
@@ -83,27 +82,27 @@ public class Final {
 		System.out.println("..... - [4] Puto       ||     ₱10   - .....");
 		System.out.println("--------------------------------------------------");
 
-		// Step 15
+		// Step 14
 		System.out.print("Enter the number of your choice: ");
 		input = reader.readLine();
 
-		// Step 16
+		// Step 15
 		try {
-			// XXX: Conversion String to Input (Step 16)
+			// XXX: Conversion String to Input (Step 15)
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
-			// Step 16
+			// Step 15
 			System.out.println("--------------------------------------------------");
 			System.out.println(">> - Invalid Input! Let me pick for you! - <<");
 			// Set input to the most expensive item.
 			selectInput = 4;
 		}
 
-		// Step 17
+		// Step 16
 		// FIXME: Use OR instead of nesting! -ken
 		selectInput = (selectInput <= 0 || selectInput > 4) ? 4 :  selectInput;
 
-		// Step 18 
+		// Step 17
 		// - FOOD -
 		pickedItem = (selectInput == 1) ? "Lechon"
 				   : (selectInput == 2) ? "Pancit"
@@ -115,43 +114,43 @@ public class Final {
 			  : (selectInput == 3) ? 15 
 			  : (selectInput == 4) ? 10 : price;
 
-		// Step 19
+		// Step 18
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^^^^^ - You picked " + pickedItem + "! - ^^^^^^");
 		System.out.println("--------------------------------------------------");
 
-		// Step 20
+		// Step 19
+		// FIXME: change much to many -ken
 		System.out.print("How many would you like: ");
 		input = reader.readLine();
 
+		// Step 20
 		try {
-			// Step 21
 			quantityInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
-			// Step 21
 			System.out.println("--------------------------------------------------");
 			System.out.println(">> - Invalid Input! Let me pick for you! - <<");
 			quantityInput = 1;
 		}
 
-		// Step 22
+		// Step 21
 		price = quantityInput * price;
-		// Step 23
+		
+		// Step 22
 		System.out.println("--------------------------------------------------");
 		System.out.println("Successfully purchased " + quantityInput + " " + pickedItem + " for ₱" + price);
 		System.out.println("--------------------------------------------------");
-		// Step 24
 		System.out.println("Please wait for your order on the counter!");
-		// Step 25
+		// Step 23
 		System.out.println("--------------------------------------------------");
 		System.out.print("         <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
 		// >>>>> ------------------- DRINKS STALL ------------------- <<<<<
-		// Step 26
+		// Step 24
 		System.out.println("> - Would you like to order some drinks? - <");
-		// Step 27
+		// Step 25
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^^^^^^^^^^^^ - Beverage Stall - ^^^^^^^^^^^^^^");
 		System.out.println("--------------------------------------------------");
@@ -161,11 +160,11 @@ public class Final {
 		System.out.println(".... - [3] Softdrinks     ||     ₱15    - ....");
 		System.out.println(".... - [4] Buko Juice     ||     ₱10    - ....");
 		System.out.println("--------------------------------------------------");
-		// Step 28
+		// Step 26
 		System.out.print("Enter the number of your choice: ");
 		input = reader.readLine();
 
-		// Step 29
+		// Step 27
 		try {
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
@@ -173,32 +172,34 @@ public class Final {
 			System.out.println(">> - Invalid Input! Let me pick for you! - <<");
 			// Default will be Iced Tea -ken
 			selectInput = 1;
-
 		}
-		// Makes sure that the user's input doesn't trigger an array
-		// If input is below minimum, set to minimum value
-		selectInput = (selectInput <= 0) ? 1 : selectInput;
-		// If input is above maximum value, set to maximum value
-		selectInput = (selectInput > 4) ? 4 : selectInput;
+		
+		// Step 28
+		selectInput = (selectInput <= 0 || selectInput > 4) ? 1 : selectInput;
 
-		// 1st Ternary sets the displayed name to the item name. 2nd sets the value to
-		// the item's.
-		pickedItem = (selectInput == 1) ? "Iced Tea" : pickedItem;
-		price = (selectInput == 1) ? 25 : price;
-		pickedItem = (selectInput == 2) ? "Mineral Water" : pickedItem;
-		price = (selectInput == 2) ? 20 : price;
-		pickedItem = (selectInput == 3) ? "Softdrinks" : pickedItem;
-		price = (selectInput == 3) ? 15 : price;
-		pickedItem = (selectInput == 4) ? "Buko Juice" : pickedItem;
-		price = (selectInput == 4) ? 10 : price;
+		// Step 29
+		// - DRINKS -
+		pickedItem = (selectInput == 1) ? "Iced Tea" : 
+					 (selectInput == 2) ? "Mineral Water" : 
+					 (selectInput == 3) ? "Softdrinks" : 
+					 (selectInput == 4) ? "Buko Juice" : pickedItem;
+		// - PRICE -
+		price = (selectInput == 1) ? 25 : 
+				(selectInput == 2) ? 20 : 
+				(selectInput == 3) ? 15 : 
+				(selectInput == 4) ? 10 : price;
 
+		// Step 30
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^^^^^ - You picked " + pickedItem + "! - ^^^^^^");
 		System.out.println("--------------------------------------------------");
 
-		// Ask the user how much they would buy
-		System.out.print("How much would you like: ");
+		// Step 31
+		// FIXME: change much to many -ken
+		System.out.print("How many would you like: ");
 		input = reader.readLine();
+		
+		// Step 32
 		try {
 			quantityInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
@@ -208,34 +209,43 @@ public class Final {
 			quantityInput = 1;
 		}
 
-		// PROCESS
+		// Step 33
 		price = quantityInput * price;
 
+		// Step 34
 		System.out.println("--------------------------------------------------");
 		System.out.println("Successfully purchased " + quantityInput + " " + pickedItem + " for ₱" + price);
+		// Step 35
 		System.out.println("--------------------------------------------------");
 		System.out.println("Please wait for your order on the counter!");
 		System.out.println("--------------------------------------------------");
+		// Step 36
 		System.out.print("          <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
-		// >>>>> ------------------- TRANSITION ------------------- <<<<<
+		// >>>>> ------------------- TRANSITION 2 ------------------- <<<<<
+		// Step 37
 		System.out.println(">>>>> - Did everyone enjoy the food? - <<<<<");
 		System.out.println("--------------------------------------------------");
+		// Step 38
 		System.out.print("          <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
-		System.out.println(">>>>> - Now that our stomach are full, - <<<<<");
+		// Step 39
+		System.out.println(">>>>> - Now that our stomachs are full, - <<<<<");
 		System.out.println("  - Let's get ready for the Sports Event!- ");
+		// Step 40
 		System.out.println("--------------------------------------------------");
 		System.out.print("          <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
 		// >>>>> ------------------- SPORTS EVENT ------------------- <<<<<
+		// Step 41
 		System.out.println(">>>>> - Welcome to the Sports Event - <<<<<");
 		System.out.println("- Pick the sport to begin with our fiesta game -");
+		// Step 42
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^^^^^^^^^ - Sports Menu - ^^^^^^^^^^^^");
 		System.out.println("--------------------------------------------------");
@@ -245,10 +255,12 @@ public class Final {
 		System.out.println("..... - [3] Badminton  [3] - .....");
 		System.out.println("..... - [4] Tennis     [4] - .....");
 		System.out.println("--------------------------------------------------");
-
+		
+		// Step 43
 		System.out.print("Enter the number of your choice: ");
 		input = reader.readLine();
-
+		
+		// Step 44
 		try {
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
@@ -258,86 +270,102 @@ public class Final {
 			selectInput = 1;
 		}
 
-		// If input is below minimum, set to minimum value
-		selectInput = (selectInput <= 0) ? 1 : selectInput;
-		// If input is above maximum value, set to maximum value
-		selectInput = (selectInput > 4) ? 4 : selectInput;
-
-		pickedItem = (selectInput == 1) ? "Basketball" : pickedItem;
-		pickedItem = (selectInput == 2) ? "Volleyball" : pickedItem;
-		pickedItem = (selectInput == 3) ? "Badminton" : pickedItem;
-		pickedItem = (selectInput == 4) ? "Tennis" : pickedItem;
-
+		// Step 45
+		selectInput = (selectInput <= 0 || selectInput > 4) ? 1 : selectInput;
+		
+		// Step 46
+		pickedItem = (selectInput == 1) ? "Basketball" : 
+					 (selectInput == 2) ? "Volleyball" : 
+					 (selectInput == 3) ? "Badminton" : 
+					 (selectInput == 4) ? "Tennis" : pickedItem;
+		// Step 47
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^ - The teams will play " + pickedItem + "! - ^^");
 		System.out.println("--------------------------------------------------");
-
+		
+		// Step 48
 		System.out.print("How many scores did Team Red get: ");
 		input = reader.readLine();
-
 		System.out.println("--------------------------------------------------");
-
+		
+		// Step 49
 		try {
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
-			System.out.println("That doesn't seem to be a number! Defaulting to 1!");
+			System.out.println(">> - Invalid Input! Let me pick for you! - <<");
 			System.out.println("--------------------------------------------------");
 			// Set to a valid value.
 			selectInput = 1;
 		}
 
+		// Step 50
 		redScore = selectInput;
 		System.out.println("Team Red scored " + redScore + "!");
 		System.out.println("--------------------------------------------------");
 
+		// Step 51
 		System.out.print("How many scores did Team Blue get: ");
 		input = reader.readLine();
 		System.out.println("--------------------------------------------------");
 
+		// Step 52
 		try {
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
-			System.out.println("That doesn't seem to be a number! Defaulting to 1!");
+			System.out.println(">> - Invalid Input! Let me pick for you! - <<");
 			System.out.println("--------------------------------------------------");
 			// Set to a valid value.
 			selectInput = 0;
 		}
 
+		// Step 53
 		blueScore = selectInput;
 		System.out.println("Team Blue scored " + blueScore + "!");
 		System.out.println("--------------------------------------------------");
 
+		// Step 54
 		System.out.println("^^^^^^^^ - AND THE WINNER IS - ^^^^^^^^^");
 		System.out.println("--------------------------------------------------");
 		System.out.print("          <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
+		// Step 55
 		scoreDifference = redScore - blueScore;
-		// Check if the value is negative(I.E: Blue got more score than Red) and change
-		// it to positive.
+		 /* TODO: Subtract the score of team red and blue and check if it's negative,
+		 * if yes change it to positive -paz
+		 * FIXME: change lead to point advantage
+		*/ 
+		
+		// Step 56
 		scoreDifference = (scoreDifference < 0) ? -scoreDifference : scoreDifference;
 
-		String result = (redScore == blueScore) ? "The match was a draw!"
-				: (redScore > blueScore)
-						? "Red Team Won the " + pickedItem + " game with a " + scoreDifference + " lead!"
-						: "Blue Team Won the " + pickedItem + " game with a " + scoreDifference + " point lead!";
+		// Step 57
+		String result = (redScore == blueScore) ? "The match was a draw!" : 
+			            (redScore > blueScore)
+						? "Red Team Won the " + pickedItem + " game with a " + scoreDifference + " point advantage!"
+						: "Blue Team Won the " + pickedItem + " game with a " + scoreDifference + " point advantage!";
 
+		// Step 58
 		System.out.println(result);
-
+		// Step 59
 		System.out.println("--------------------------------------------------");
 		System.out.print("          <-> - (Press Enter) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
 		// >>>>> ------------------- RAFFLE ------------------- <<<<<
+		// Step 60
 		System.out.println(" - You are eligible for a raffle prize! - ");
 		System.out.println("--------------------------------------------------");
 		System.out.println(">>>>> - Select a raffle ticket(1-9) - <<<<<");
 		System.out.println("--------------------------------------------------");
+		
+		// Step 61
 		System.out.print("Pick a number : ");
-
 		input = reader.readLine();
+		
+		// Step 62
 		try {
 			selectInput = Integer.parseInt(input);
 		} catch (NumberFormatException e) {
@@ -345,37 +373,44 @@ public class Final {
 			selectInput = 1;
 		}
 
+		// Step 63
+		// FIXME: it should be greater than 9 not 4 -ken
 		// If input is below minimum, set to minimum value
-		selectInput = (selectInput <= 0) ? 1 : selectInput;
+		selectInput = (selectInput <= 0 || selectInput > 9) ? 1 : selectInput;
+		
+		// Step 64
 		// If input is above maximum value, set to maximum value
-		selectInput = (selectInput > 4) ? 4 : selectInput;
-
-		pickedItem = (selectInput == 1) ? "Ticket to Saja Boys" : pickedItem;
-		pickedItem = (selectInput == 2) ? "Tumbler" : pickedItem;
-		pickedItem = (selectInput == 3) ? "Fiesta Hat" : pickedItem;
-		pickedItem = (selectInput == 4) ? "Electric Fan" : pickedItem;
-		pickedItem = (selectInput == 5) ? "Laptop" : pickedItem;
-		pickedItem = (selectInput == 6) ? "Smart Tv" : pickedItem;
-		pickedItem = (selectInput == 7) ? "Brand New Car" : pickedItem;
-		pickedItem = (selectInput == 8) ? "House and Lot" : pickedItem;
-		pickedItem = (selectInput == 9) ? "Iphone" : pickedItem;
-
+		pickedItem = (selectInput == 1) ? "Ticket to Saja Boys" : 
+				 	 (selectInput == 2) ? "Tumbler" : 
+					 (selectInput == 3) ? "Fiesta Hat" : 
+					 (selectInput == 4) ? "Electric Fan" : 
+					 (selectInput == 5) ? "Laptop" :
+					 (selectInput == 6) ? "Smart Tv" : 
+					 (selectInput == 7) ? "Brand New Car" : 
+					 (selectInput == 8) ? "House and Lot" : 
+					 (selectInput == 9) ? "Iphone" : pickedItem;
+		
+		// Step 65
 		System.out.println("--------------------------------------------------");
 		System.out.println("^^ > 🎁 You won a " + pickedItem + "! < ^^");
 		System.out.println("--------------------------------------------------");
+		// Step 66
 		System.out.print("      <-> - (Press Enter To Continue) - <->   ");
 		reader.readLine();
 		System.out.println("--------------------------------------------------");
 
 		// >>>>> ------------------- EXIT ------------------- <<<<<
+		// Step 67
 		System.out.println("🎉 Maraming salamat sa lahat ng dumalo! 🎉");
 		System.out.println("❤️ - Kita-kits sa susunod na Fiesta! - ❤️");
 		System.out.println(" ^^^^ Sponsored by Mayor Losamia! ^^^^");
 		System.out.println("--------------------------------------------------");
+		// Step 69
 		System.out.print("      <-> - (Press Enter To Continue) - <->   ");
 		reader.readLine();
 
 		// >>>>> ------------------- CREDITS ------------------- <<<<<
+		// Step 68
 		System.out.println("==================================================");
 		System.out.println("^^^^^ - FIESTA SIMULATOR - ^^^^^     ");
 		System.out.println("       >> - MEMBERS - <<             ");
