24d58c1e3c32275dedeaeebb0de0a6c1307aaa9f
x40arcade <sobrepenakenneth97@gmail.com>
2025-10-06 20:49:11 +0800

    MidTerm #1

---
+++ CCS_102_MidTerm/src/For_Loops/BasicForLoop.java
@@ -0,0 +1,18 @@
+package For_Loops;
+
+public class BasicForLoop {
+
+	public static void main(String[] args) {
+		// For Loops
+		// Basic Syntax
+		/* for (initialization; condition; increment/decrement;) {
+		 * 		Statement
+		 * }
+		 * */
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Number: " + i);
+		}
+		
+	} // main method
+}// class
---
+++ CCS_102_MidTerm/src/For_Loops/EvenNumbers.java
@@ -0,0 +1,9 @@
+package For_Loops;
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+		for (int i = 2; i <= 10; i += 2) {
+			System.out.println("Even Numbers: " + i);
+		} // For Loop
+	} // main method 
+} // class
