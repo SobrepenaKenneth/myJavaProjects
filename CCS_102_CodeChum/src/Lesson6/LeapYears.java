48a5bac3031bb98aa9c3e758c49018055f355afa
x40arcade <sobrepenakenneth97@gmail.com>
2025-10-08 20:10:29 +0800

    CodeChum_Lesson6_LeapYear

--- CCS_102_CodeChum/src/Lesson6/LeapYears.java
+++ CCS_102_CodeChum/src/Lesson6/LeapYears.java
@@ -6,14 +6,36 @@ public class LeapYears {
 
 	public static void main(String[] args) {
 		/* 
-		 * LESSON 6: EQUALITY
+		 * LESSON 6: Leap Years
 		 * by codechum Admin
 		 * 
-		 * Write a program that takes two integers, x and y, 
-		 * as input and checks if they are equal, if x is equal to y,
-		 * it print "Numbers are equal."
+		 * Write a program that takes an intenger year as input and performs the following
+		 * steps to determine if the year is a leap year:
+		 * 
+		 * 1. Checks if year is divisible by 4 (i.e., year % 4 == 0) to check if it's a
+		 *    multiple of 4.
+		 * 2. Checks if year is not divisible by 100 (i.e., year % 100 != 0) to exclude years
+		 *    that are multiples of 100 but not multiples of 400.
+		 * 3. CHecks if year is divisible by 400 (i.e., year % 400 == 0) to include years that
+		 *    are multiples of 400.
+		 *        > Input: An Integer, year, representing a year.
+		 * 
+		 * The program should print "Year is a leap year." if condition is met. Otherwise, do
+		 * nothing.
 		 * */
+		
+		Scanner scan = new Scanner(System.in);
+		// > Declaration
+		int year;
+		
+		// > Input
+		System.out.println("Enter the year: ");
+		year = scan.nextInt();
+		
+		// > Process & Output
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			System.out.println("Year is a leap year.");
+		}
 
 	}
-
 }
