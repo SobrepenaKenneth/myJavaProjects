3c33586a76785a18e2db73b8c5b390c2e6153d64
x40arcade <sobrepenakenneth97@gmail.com>
2025-11-07 10:49:31 +0800

    07112025

--- CCS_102_CodeChum/src/DoWhileLoop/Counting.java
+++ CCS_102_CodeChum/src/DoWhileLoop/Counting.java
@@ -7,17 +7,11 @@ public class Counting {
 	 * Write a program that takes an integer n. Use a do-while loop to count from 1
 	 * to n (inclusive), and print each number on a new line.
 	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n, counter = 0;
-
-		System.out.print("Enter a number: ");
-		n = scan.nextInt();
-
-		do {
-			counter++;
-			System.out.println(counter);
-		} while (counter != n);
-		scan.close();
+	public static void main(String[] args) {int i = 1;
+		while(i <= 5) {
+			i++;
+			if (i == 3) continue;
+			System.out.print(i + " ");
+		}
 	}
 }
