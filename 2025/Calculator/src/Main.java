82113e5bd95038ac8215f937321461f4b8a71bae
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-04 10:37:59 +0800

    Reorganize project directories under 2025 folder
    Moved multiple Java project directories and files from various locations (such as DUMP, PNC, and others) into a unified '2025' directory structure. This change consolidates project organization, making it easier to manage and maintain related coursework and code samples.

---
+++ 2025/Calculator/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void showMenu() {
+		int input;
+		
+		System.out.println("=== CONSOLE CALCULATOR ===");
+		System.out.println(":=> [1] Addition          ");
+		System.out.println(":=> [2] Subtraction       ");
+		System.out.println(":=> [3] Multiplication    ");
+		System.out.println(":=> [4] Division          ");
+		System.out.println("==========================");
+		input = scan.nextInt();
+		
+		
+	}
+
+}
