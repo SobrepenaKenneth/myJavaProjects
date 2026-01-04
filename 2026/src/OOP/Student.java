783d90f779b21b66c721016ab8cd01a340926990
x40arcade <sobrepenakenneth97@gmail.com>
2026-01-04 12:17:20 +0800

    Add Student and Person classes with details printing
    Introduces a base Person class and a Student class with attributes for name, roll number, and section. Includes a method to print student details and a main method demonstrating usage.

---
+++ 2026/src/OOP/Student.java
@@ -0,0 +1,39 @@
+package OOP;
+
+// base class for all students
+class Person {
+	String name;
+	
+	Person(String name) {
+		this.name = name;
+	}
+}
+
+// This is a student class
+public class Student {
+	// Declaring Attributes
+	String name;
+	int rollNo;
+	String section;
+	
+	// initializing Attributes
+	Student(String name, int rollNo, String section) {
+		this.name = name;
+		this.rollNo = rollNo;
+		this.section = section;
+	}
+	
+	// print details
+	public void printDetails() {
+		System.out.println("Student Details:");
+		System.out.print(this.name + ", " + this.rollNo + ", " + section);
+	}
+	
+	public static void main(String[] args) {
+		Student student1 = new Student("Felonia", 2, "1CS-A");
+		Student student2 = new Student("Kenneth", 1, "1CS-A");
+		
+		student1.printDetails();
+		student2.printDetails();
+	}
+}
