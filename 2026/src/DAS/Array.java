aabde0784d97a32c9d23a56d4ef79ce6b1b74584
x40arcade <sobrepenakenneth97@gmail.com>
2026-06-15 20:29:04 +0800

    Linear search algorithm

--- 2026/src/DAS/Array.java
+++ 2026/src/DAS/Array.java
@@ -10,8 +10,8 @@ public class Array {
 		//arrayBasics();
 		//accessingValue();
 		//readingInputs();
-		linearSearch();
-
+//		linearSearch();
+		linearSearch2();
 	}
 	
 	public static void arrayBasics() {
@@ -55,7 +55,7 @@ public class Array {
 		String[] names = new String[5];
 		
 		for (int index = 0; index < names.length; index++) {
-			System.out.println("Enter a name: ");
+			System.out.print("Enter a name: ");
 			names[index] = reader.readLine();
 		}
 		
@@ -64,6 +64,8 @@ public class Array {
 		}
 	}
 	
+	// Searching a value
+	
 	// Linear Search attempt
 	// i don't much about it but let's say we have 10 numbers and we are going to look
 	// for a specific number
@@ -71,18 +73,76 @@ public class Array {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int[] numberList = {2, 5, 6, 4, 3, 8, 9, 7, 10, 12, 54, 67};
 		
-		int index = 0;
-		int number = 0;
+		boolean found = false;	
 		
-		System.out.println("Search number: ");
-		number = Integer.parseInt(reader.readLine());
+		System.out.print("Search number: ");
+		int number = Integer.parseInt(reader.readLine());
 		
 		for (int i = 0; i < numberList.length; i++) {
-			if (number != numberList[i]) {
-				index++;
-				continue;
+			if (number == numberList[i]) {
+				System.out.println("Number found at index " + i + ": " + number);
+				found = true;
+				break;
+			}
+		}
+		
+		if (!found) {
+			System.out.println("Number " + number + " is not in the array!");
+		}
+	}
+	
+	// Big 0(n) Linear Search
+	
+	public static void linearSearch2() throws IOException {
+		// The size of an array
+		int size = 0;
+		
+		// The number to be searched
+		int numToSearch;
+		
+		// if true, it means that numToSearch has been found
+		boolean found = false;
+		boolean error = false;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		// Set the size of the array
+		try {
+			System.out.print("Enter the size of the array: ");
+			size = Integer.parseInt(reader.readLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Enter an integer only!");
+			error = true;
+		}
+		
+		if (error == true) {
+			System.out.println("Please try again!");
+		} else {
+			// Crate the array
+			int[] array = new int[size];
+			
+			// assign a value to the array
+			for (int i=0; i < size; i++) {
+				System.out.print("Enter a number: ");
+				array[i] = Integer.parseInt(reader.readLine());
+			}
+			
+			// set a value to numSearch
+			System.out.print("Enter the number to be searched: ");
+			numToSearch = Integer.parseInt(reader.readLine());
+			
+			// apply linear search algorithm
+			for (int i=0; i<size; i++) {
+				if (numToSearch == array[i]) {
+					found = true;
+					break;
+				}
+			}
+			
+			if (found == true) {
+				System.out.println("The number is present in the array!");
 			} else {
-				System.out.println("Number found at " + index + " index: " + number);
+				System.out.println("The number is not present in the array!");
 			}
 		}
 	}
